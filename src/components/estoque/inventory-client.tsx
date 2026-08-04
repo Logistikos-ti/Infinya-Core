21e324f05f679bc6bdd3764d764acf0d98125fd2
Logistikos-ti <ti@logistikos.com.br>
2026-08-04 08:39:31 -0300

    refactor: keep stock movements in product detail

--- src/components/estoque/inventory-client.tsx
+++ src/components/estoque/inventory-client.tsx
@@ -10,7 +10,6 @@ import { InventoryTableLot } from "./inventory-table-lot";
 import { InventoryAlerts } from "./inventory-alerts";
 import { InventoryDetailDrawer } from "./inventory-detail-drawer";
 import { InitialStockModal } from "./initial-stock-modal";
-import { InventoryRecentMovements } from "./inventory-recent-movements";
 
 const INVENTORY_VIEW_STATE_KEY = "infinoos-wms:inventory-view";
 
@@ -230,7 +229,6 @@ export function InventoryClient({ data }: { data: any }) {
         <InventoryTableLot t={t} balances={filteredBalances} />
       )}
 
-      <InventoryRecentMovements t={t} movements={data.stockMovements || []} />
       <InventoryAlerts t={t} alerts={data.stockExpiryAlerts} />
 
       {selectedSku && (
